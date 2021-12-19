6bdb9091d64dc4f096b3ee2ea306e7dceef4719e
knguyen111601 <knguyen111601@gmail.com>
2021-12-19 12:46:26 -0800

    Added the signup route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resource :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+  post "/signup" to: "users#create"
 
   root 'pages#index'
   namespace :api do
